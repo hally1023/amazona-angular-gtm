c872500c8d470e547b0bac92cd0fb30556849e35
Ejaz Ahmed <ejazahmed.dev@gmail.com>
2021-07-15 19:09:02 +0500

    feat: added screens, header, footer

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './screens/cart/cart.component';
+import { HomeComponent } from './screens/home/home.component';
+import { OrderHistoryComponent } from './screens/order-history/order-history.component';
+import { OrderComponent } from './screens/order/order.component';
+import { PaymentMethodComponent } from './screens/payment-method/payment-method.component';
+import { PlaceOrderComponent } from './screens/place-order/place-order.component';
+import { ProductEditComponent } from './screens/product-edit/product-edit.component';
+import { ProfileComponent } from './screens/profile/profile.component';
+import { RegisterComponent } from './screens/register/register.component';
+import { ShippingAddressComponent } from './screens/shipping-address/shipping-address.component';
+import { SignInComponent } from './screens/sign-in/sign-in.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cart/:id', component: CartComponent },
+  { path: 'product/:id/edit', component: ProductEditComponent },
+  { path: 'signin', component: SignInComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'shipping', component: ShippingAddressComponent },
+  { path: 'payment', component: PaymentMethodComponent },
+  { path: 'order/:id', component: OrderComponent },
+  { path: 'show-order/:id', component: OrderComponent },
+  { path: 'thank-you/:id', component: OrderComponent },
+  { path: 'order-complete/:id', component: OrderComponent },
+  { path: 'refund-order/:id', component: OrderComponent },
+  { path: 'orderhistory', component: OrderHistoryComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: '', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,9 +9,22 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CheckoutStepsComponent } from './components/checkout-steps/checkout-steps.component';
 import { LoadingBoxComponent } from './components/loading-box/loading-box.component';
 import { MessageBoxComponent } from './components/message-box/message-box.component';
+import { HomeComponent } from './screens/home/home.component';
+import { CartComponent } from './screens/cart/cart.component';
+import { OrderHistoryComponent } from './screens/order-history/order-history.component';
+import { OrderListComponent } from './screens/order-list/order-list.component';
+import { OrderComponent } from './screens/order/order.component';
+import { PaymentMethodComponent } from './screens/payment-method/payment-method.component';
+import { PlaceOrderComponent } from './screens/place-order/place-order.component';
+import { ProductEditComponent } from './screens/product-edit/product-edit.component';
+import { ProductListComponent } from './screens/product-list/product-list.component';
+import { ProfileComponent } from './screens/profile/profile.component';
+import { RegisterComponent } from './screens/register/register.component';
+import { ShippingAddressComponent } from './screens/shipping-address/shipping-address.component';
+import { SignInComponent } from './screens/sign-in/sign-in.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductComponent, RatingComponent, CheckoutStepsComponent, LoadingBoxComponent, MessageBoxComponent],
+  declarations: [AppComponent, ProductComponent, RatingComponent, CheckoutStepsComponent, LoadingBoxComponent, MessageBoxComponent, HomeComponent, CartComponent, OrderHistoryComponent, OrderListComponent, OrderComponent, PaymentMethodComponent, PlaceOrderComponent, ProductEditComponent, ProductListComponent, ProfileComponent, RegisterComponent, ShippingAddressComponent, SignInComponent],
   imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
