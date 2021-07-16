3da9b0d5b4c63d239b8c6a9ea1836a489ba8b2d4
Ejaz Ahmed <ejazahmed.dev@gmail.com>
2021-07-16 09:17:10 +0500

    feat: added details product page

--- frontend/src/app/actions/product/details-product.actions.spec.ts
+++ frontend/src/app/actions/product/details-product.actions.spec.ts
@@ -2,7 +2,7 @@ import * as fromDetailsProduct from './details-product.actions';
 
 describe('DetailsProducts', () => {
   it('should return an action', () => {
-    expect(fromDetailsProduct.DetailsProducts().type).toBe(
+    expect(fromDetailsProduct.detailsProducts({ productId: '' }).type).toBe(
       '[DetailsProduct] Details Products'
     );
   });
--- frontend/src/app/screens/product-details/product-details.component.ts
+++ frontend/src/app/screens/product-details/product-details.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.scss']
+  styleUrls: ['./product-details.component.scss'],
 })
 export class ProductDetailsComponent implements OnInit {
+  product: Product | null = null;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
