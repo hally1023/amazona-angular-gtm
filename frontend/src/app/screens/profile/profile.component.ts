fb98c711e7d3d26a051f8500cc4f749507d13797
Ejaz Ahmed <ejazahmed.dev@gmail.com>
2021-07-17 19:38:24 +0500

    feat: added profile component

--- frontend/src/app/screens/profile/profile.component.ts
+++ frontend/src/app/screens/profile/profile.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { userDetails } from 'src/app/actions/user/details.actions';
+import { State } from 'src/app/reducers';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  constructor(private store: Store<State>) {}
 
   ngOnInit(): void {
+    this.store
+      .select((state) => state.userAuth)
+      .subscribe((userAuth) => {
+        if (userAuth.userInfo) {
+          this.store.dispatch(userDetails({ userId: userAuth.userInfo._id }));
+        }
+      });
   }
-
 }
