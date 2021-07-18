be3666d9858a8df52bd957b15664e8d2af209c0d
Ejaz Ahmed <ejazahmed.dev@gmail.com>
2021-07-18 14:24:51 +0500

    feat: payment method works

--- frontend/src/app/screens/payment-method/payment-method.component.ts
+++ frontend/src/app/screens/payment-method/payment-method.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { cartSavePaymentMethod } from 'src/app/actions/cart/cart.actions';
+import { PaymentMethod } from 'src/app/models/payment-method.model';
+import { State } from 'src/app/reducers';
 
 @Component({
   selector: 'app-payment-method',
   templateUrl: './payment-method.component.html',
-  styleUrls: ['./payment-method.component.scss']
+  styleUrls: ['./payment-method.component.scss'],
 })
 export class PaymentMethodComponent implements OnInit {
+  paymentMethod: PaymentMethod = 'PayPal';
 
-  constructor() { }
+  submitHandler() {
+    this.store.dispatch(
+      cartSavePaymentMethod({ paymentMethod: this.paymentMethod })
+    );
 
-  ngOnInit(): void {
+    this.router.navigate(['/placeorder']);
   }
 
+  constructor(private router: Router, private store: Store<State>) {}
+
+  ngOnInit(): void {
+    this.store
+      .select((state) => state.cart.shippingAddress)
+      .subscribe((shippingAddress) => {
+        if (!shippingAddress) {
+          this.router.navigate(['/shipping']);
+        }
+      });
+  }
 }
