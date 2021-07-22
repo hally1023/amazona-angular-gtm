e086b2fab5115023fcb6147181803e96a13e3f1e
Ejaz Ahmed <ejazahmed.dev@gmail.com>
2021-07-22 13:07:56 +0500

    fix: order history completed

--- frontend/src/app/screens/order-history/order-history.component.ts
+++ frontend/src/app/screens/order-history/order-history.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { orderMineList } from 'src/app/actions/order/mine-list.actions';
+import { Order } from 'src/app/models/order.model';
+import { State } from 'src/app/reducers';
 
 @Component({
   selector: 'app-order-history',
   templateUrl: './order-history.component.html',
-  styleUrls: ['./order-history.component.scss']
+  styleUrls: ['./order-history.component.scss'],
 })
 export class OrderHistoryComponent implements OnInit {
+  loading: boolean | undefined;
+  error: any;
+  orders: Order[] = [];
 
-  constructor() { }
+  detailsHandler = (orderId: string) => {
+    this.router.navigate([`/order/${orderId}`]);
+  };
+
+  constructor(private router: Router, private store: Store<State>) {}
 
   ngOnInit(): void {
-  }
+    this.store.dispatch(orderMineList());
 
+    this.store
+      .select((state) => state.orderMineList)
+      .subscribe((orderMineList) => {
+        this.loading = orderMineList.loading;
+        this.error = orderMineList.error;
+        this.orders = orderMineList.orders!;
+      });
+  }
 }
